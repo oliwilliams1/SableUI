446baf20cdcb1a79c152d03ba084bd21df368d25
Oli Williams <imhungryoliver@gmail.com>
2025-05-01 17:09:16 +1200

    SDL2 linked and running correctly

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
-#include <SDL3/SDL.h>
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
 #include <vector>
-#include <cstdio> // For printf
+#include <cstdio>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
-int main() {
+int main(int argc, char* argv[])
+{
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Pixel Buffer", WIDTH, HEIGHT, 0);
+    SDL_Window* window = SDL_CreateWindow("Pixel Buffer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
@@ -19,22 +21,20 @@ int main() {
     }
 
     SDL_Surface* surface = SDL_GetWindowSurface(window);
-    std::vector<uint32_t> pixelBuffer(WIDTH * HEIGHT, 0xFF000000); // Black
+    std::vector<uint32_t> pixelBuffer(WIDTH * HEIGHT, 0xFF000000);
 
-    // Modify pixel buffer (example: fill with red)
     for (int y = 0; y < HEIGHT; ++y) {
         for (int x = 0; x < WIDTH; ++x) {
-            pixelBuffer[y * WIDTH + x] = 0xFFFF0000; // Red
+            pixelBuffer[y * WIDTH + x] = 0xFFFF0000;
         }
     }
 
-    // Lock the surface to access pixel data
     SDL_LockSurface(surface);
     std::memcpy(surface->pixels, pixelBuffer.data(), pixelBuffer.size() * sizeof(uint32_t));
     SDL_UnlockSurface(surface);
 
     SDL_UpdateWindowSurface(window);
-    SDL_Delay(3000); // Show for 3 seconds
+    SDL_Delay(3000);
 
     SDL_DestroyWindow(window);
     SDL_Quit();
